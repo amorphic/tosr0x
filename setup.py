b0f74ef347bda9d7ee431606692f0fa4ca2bd755
James Stewart <jstewart101@gmail.com>
2013-08-30 20:54:26 +1000

    Update setup.py
    removed numpy requirement

--- setup.py
+++ setup.py
@@ -5,8 +5,5 @@ setup(
     version='0.1.0',
     py_modules=['tosr0x'],
     description='An interface to the tinysine.com tosr0x line of USB-controlled relays.',
-    long_description=open('README.md').read(),
-    install_requires=[
-        "numpy",
-    ],
+    long_description=open('README.md').read()
 )
