6ebde201a9c05018f9620cdd733a4c08867e6ec2
James Stewart <jstewart101@gmail.com>
2013-08-30 20:53:00 +1000

    Update setup.py
    removed leftover windows code

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 from distutils.core import setup
 
-#Fix Windows Binary Build
-import codecs 
-try: 
-    codecs.lookup('mbcs') 
-except LookupError: 
-    ascii = codecs.lookup('ascii') 
-    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs') 
-    codecs.register(func) 
-
 setup(
     name='tosr0x',
     version='0.1.0',
     py_modules=['tosr0x'],
-    description='An interface to the tinysine.com line of USB-controlled relays.',
+    description='An interface to the tinysine.com tosr0x line of USB-controlled relays.',
     long_description=open('README.md').read(),
     install_requires=[
         "numpy",
