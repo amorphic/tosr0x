b30751e0ae8da50a5947f2ac0054a831677801be
James Stewart <jstewart101@gmail.com>
2014-01-27 23:34:28 +1100

    Updates for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 
 setup(
     name='tosr0x',
-    version='0.1.0',
+    version='0.2.0',
+    author='James Stewart',
+    author_email='jstewart101@gmail.com',
+    url='https://github.com/amorphic/tosr0x',
     py_modules=['tosr0x'],
     description='An interface to the tinysine.com tosr0x line of USB-controlled relays.',
     long_description=open('README.md').read()
